96fe37bbb00bf17fe82e51a2bee2069b9a6354ba
Hanna-Ismahilova <hi.infoshare@gmail.com@gmail.com>
2020-04-17 19:12:27 +0200

    Linked the pages

---
+++ AutomationHIVE/Pages/Mentee/MenteeList.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AutomationCodeHive.Pages.Mentee
+{
+    public class MenteeListModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
